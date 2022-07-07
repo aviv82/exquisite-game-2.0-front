3f6f2e730ca55ed1bb9c5db25979002868512cc3
Aviv <aviv.dror@gmail.com>
2022-07-07 21:06:14 +0200

    better update

--- src/handlers/handlePut.js
+++ src/handlers/handlePut.js
@@ -1,12 +1,13 @@
 import { put } from "../api/put";
 
 export const handlePut = async (event) => {
+  const toSearch = "tests/1";
   const toUpdate = {
     data: {
       field: event.target.parentElement.children[0].value,
     },
   };
-  const updatePromise = await put("tests/1", toUpdate);
+  const updatePromise = await put(toSearch, toUpdate);
   console.log("update test", updatePromise);
   event.target.parentElement.children[0].value = "";
   return updatePromise;
