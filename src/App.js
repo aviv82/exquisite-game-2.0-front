14a871d88f082aed9410f3a2b43b04d6e5fc9c04
Aviv <aviv.dror@gmail.com>
2022-07-07 21:23:47 +0200

    post button

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import { Button } from "./component/button/Button";
 
 import { handleGet } from "./handlers/handleGet";
+import { handlePost } from "./handlers/handlePost";
 import { handlePut } from "./handlers/handlePut";
 
 function App() {
   return (
     <div className="App">
       <div className="api tests">
-        <input type="input"></input>
+        <input type="input" placeholder="value"></input>
         <Button kind="button" title="get" face="green" action={handleGet} />
         <Button kind="button" title="update" face="yellow" action={handlePut} />
+        <Button kind="button" title="add" face="blue" action={handlePost} />
       </div>
     </div>
   );
---
+++ src/handlers/handlePost.js
@@ -0,0 +1,15 @@
+import { post } from "../api/post";
+
+export const handlePost = async (event) => {
+  const toAdd = "tests";
+  const inputValue = event.target.parentElement.children[0].value;
+  const addValue = {
+    data: {
+      field: inputValue,
+    },
+  };
+  const addPromise = await post(toAdd, addValue);
+  console.log("test post", addPromise);
+  event.target.parentElement.children[0].value = "";
+  return addPromise;
+};
