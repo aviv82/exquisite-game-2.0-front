2a3255a3023595f2345956b754b0959e4b06b424
Aviv <aviv.dror@gmail.com>
2022-07-09 23:10:18 +0200

    setup signup form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
-import { useState, useRef } from "react";
+// import { useState, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Button } from "./component/button/Button";
 
@@ -10,18 +11,35 @@ import { handlePut } from "./handlers/handlePut";
 import { handleDelete } from "./handlers/handleDelete";
 import { handleAuth } from "./handlers/handleAuth";
 
+import { Home } from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <div className="api tests">
-        <input type="input" placeholder="value"></input>
-        <Button kind="button" title="get" face="green" action={handleGet} />
-        <Button kind="button" title="update" face="yellow" action={handlePut} />
-        <Button kind="button" title="add" face="blue" action={handlePost} />
-        <Button kind="button" title="delete" face="red" action={handleDelete} />
-        <Button kind="button" title="auth" face="pink" action={handleAuth} />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <div className="api tests">
+          <input type="input" placeholder="value"></input>
+          <Button kind="button" title="get" face="green" action={handleGet} />
+          <Button
+            kind="button"
+            title="update"
+            face="yellow"
+            action={handlePut}
+          />
+          <Button kind="button" title="add" face="blue" action={handlePost} />
+          <Button
+            kind="button"
+            title="delete"
+            face="red"
+            action={handleDelete}
+          />
+          <Button kind="button" title="auth" face="pink" action={handleAuth} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,102 @@
+import "./Home.css";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { ref, object, string, boolean } from "yup";
+
+import { Button } from "../component/button/Button";
+
+export const Home = () => {
+  return (
+    <Formik
+      initialValues={{
+        exquisite: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+      }}
+      validationSchema={object({
+        exquisite: string().required("Required Field"),
+        email: string().required("Required Field"),
+        password: string()
+          .required("Required Field")
+          .min(4, "Password must be longer than four digits")
+          .max(20, "Password must be shorter than twenty digits"),
+        passwordConfirmation: string()
+          .oneOf([ref("password"), null], "Passwords must match")
+          .required("Required Field"),
+        isWorking: boolean().oneOf([false, true]),
+      })}
+      onSubmit={(values, onSubmitProps) => {
+        console.log("values", values);
+        onSubmitProps.resetForm();
+      }}
+    >
+      {({ values, isSubmitting, errors }) => (
+        <Form className="sign-up-form">
+          <div className="sign-up-name">
+            <label htmlFor="exquisite">Exquisite Name:</label>
+            <Field
+              name="exquisite"
+              type="text"
+              placeholder="Your Exquisite Name"
+            ></Field>
+            <ErrorMessage
+              component="div"
+              className="error"
+              name="exquisite"
+            ></ErrorMessage>
+          </div>
+          <div className="sign-up-email">
+            <label htmlFor="email">Exquisite Email:</label>
+            <Field
+              name="email"
+              type="email"
+              placeholder="Exquisite Email"
+            ></Field>
+            <ErrorMessage
+              component="div"
+              className="error"
+              name="email"
+            ></ErrorMessage>
+          </div>
+          <div className="sign-up-pass">
+            <label htmlFor="password">Exquisite Password:</label>
+            <Field
+              name="password"
+              type="password"
+              placeholder="Exquisite Password"
+            ></Field>
+            <ErrorMessage
+              component="div"
+              className="error"
+              name="password"
+            ></ErrorMessage>
+          </div>
+          <div className="sign-up-pass-confirm">
+            <label htmlFor="password">Password Confirm:</label>
+            <Field
+              name="passwordConfirmation"
+              type="password"
+              placeholder="Password Confirm"
+            ></Field>
+            <ErrorMessage
+              component="div"
+              className="error"
+              name="passwordConfirmation"
+            ></ErrorMessage>
+          </div>
+          <div>
+            <Button
+              kind="submit"
+              title="Sign Up"
+              face="blue"
+              disabled={isSubmitting}
+            />
+          </div>
+          <pre>{JSON.stringify(values, null, 4)}</pre>
+          <pre>{JSON.stringify(errors, null, 4)}</pre>
+        </Form>
+      )}
+    </Formik>
+  );
+};
