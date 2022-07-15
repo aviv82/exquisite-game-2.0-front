89ef1b6cffa009b5b1a68abb05363a2cbc0dccb5
Aviv <aviv.dror@gmail.com>
2022-07-15 02:26:18 +0200

    lobby setup

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { handleAuth } from "./handlers/handleAuth";
 import { Home } from "./pages/Home/Home";
 import { SignUp } from "./pages/signup/SignUp";
 import { LogIn } from "./pages/login/LogIn";
+import { Lobby } from "./pages/lobby/Lobby";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/lobby" element={<Lobby />} />
         </Routes>
         <div className="api tests">
           <input type="input" placeholder="value"></input>
---
+++ src/pages/lobby/Lobby.jsx
@@ -0,0 +1,5 @@
+import "./Lobby.css";
+
+export const Lobby = () => {
+  return <h2 className="lobby-head">Welcome to the Exquisite Lobby!</h2>;
+};
