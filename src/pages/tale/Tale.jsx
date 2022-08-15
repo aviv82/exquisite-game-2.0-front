6d7a598b74e310c6c5a07674cf58241e228d1aa8
Aviv <aviv.dror@gmail.com>
2022-08-15 20:56:47 +0200

    tale display

--- src/pages/tale/Tale.jsx
+++ src/pages/tale/Tale.jsx
@@ -30,13 +30,13 @@ export const Tale = () => {
     }
   }, []);
 
-  console.log("cookie test", Cookies.get("tale"), "tales", tales.data);
+  // console.log("cookie test", Cookies.get("tale"), "tales", tales.data);
 
   if (tales.data !== undefined) {
     taleToShow.push(
       tales.data.filter((tale) => tale.id === Number(Cookies.get("tale")))
     );
-    console.log("this tale", taleToShow[0][0]);
+    // console.log("this tale", taleToShow[0][0]);
   }
 
   return !Cookies.get("cookieConfirm") ? (
@@ -49,11 +49,25 @@ export const Tale = () => {
         action={handleCookieConfirm}
       ></Button>
     </div>
-  ) : tales.data === undefined ? (
+  ) : tales.data === undefined || taleToShow[0][0] === undefined ? (
     <h1>Loading...</h1>
   ) : (
     <div className="tale-wrap">
-      <h2 className="tale-test">tales test</h2>
+      <h2 className="tale-title">{taleToShow[0][0].attributes.title}</h2>
+      <h4 className="tale-authors">
+        Written by{" "}
+        {taleToShow[0][0].attributes.creators.data[0].attributes.username}
+        {taleToShow[0][0].attributes.contributor.data.map((name, i) =>
+          i === taleToShow[0][0].attributes.contributor.data.length - 1
+            ? String(` & ${name.attributes.username}`)
+            : String(`, ${name.attributes.username}`)
+        )}
+      </h4>
+      {taleToShow[0][0].attributes.segment.map((seg) => (
+        <p key={seg.id} className="tale-segment">
+          {seg.body}
+        </p>
+      ))}
       <div className="return-to-library" onClick={handleClearTaleCookie}>
         <Link to="/library">
           <Button kind="submit" title="return to library" face="red"></Button>
