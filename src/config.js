6f6726aa700e008e94e5f5c04313a992ca4656c0
Aviv <89319835+aviv82@users.noreply.github.com>
2022-11-25 14:13:19 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const ORIGIN = "https://the-exquisite-game.herokuapp.com/api/";
+// export const ORIGIN = "https://the-exquisite-game.herokuapp.com/api/";
+export const ORIGIN = "https://exquisite-game-back-production.up.railway.app/api/";
